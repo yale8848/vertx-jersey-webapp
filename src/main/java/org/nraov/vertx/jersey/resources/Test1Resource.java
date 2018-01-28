111c54283dd22594e8d56a268a1972e58fb22086
yale8848 <royal8848@163.com>
2018-01-28 12:11:54 +0800

    add params

--- src/main/java/org/nraov/vertx/jersey/resources/Test1Resource.java
+++ src/main/java/org/nraov/vertx/jersey/resources/Test1Resource.java
@@ -1,17 +1,19 @@
 package org.nraov.vertx.jersey.resources;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.streams.ReadStream;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.net.ssl.SSLSession;
+import javax.ws.rs.*;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.io.Serializable;
 
 @Path("/test1")
 @Produces(MediaType.APPLICATION_JSON)
@@ -19,8 +21,18 @@ public class Test1Resource {
 
 	@GET
 	@Path("id")
-	public String getId() {
-		return "Id from Test1Resource";
+	public Response getId(@Context Vertx vertx,
+						  @Context HttpServerRequest vertxRequest,
+						  @Context HttpServerResponse vertxResponse,
+						  @Context ReadStream<HttpServerRequest> vertxStream,
+						  @CookieParam("name") String value) {
+		SSLSession sslSession = vertxRequest.sslSession();
+		MyObject o = new MyObject();
+		o.setName("Andy");
+		return Response.ok(o)
+				.cookie(new NewCookie("name", "Helloworld!"))
+				.build();
+		//return "Id from Test1Resource "+id;
 	}
 	@GET
 	@Path("json")
